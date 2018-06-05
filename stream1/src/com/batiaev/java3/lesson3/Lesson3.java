c47ba8dea8acd67bfa5eaa76b4da7ad4f26ef2a1
Anton Batiaev <a.batyaev@gmail.com>
2018-06-05 14:10:18 +0300

    add homework3 and lesson4

--- stream1/src/com/batiaev/java3/lesson3/Lesson3.java
+++ stream1/src/com/batiaev/java3/lesson3/Lesson3.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Lesson3 {
     public static void main(String[] args) {
 //        file();
-//        byteArrayStream();
+        byteArrayStream();
 //        fileStream();
 //        pipedStream();
 //        sequenceStream();
