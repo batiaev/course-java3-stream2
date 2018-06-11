67a2e617a635abba6df3f4d4169e332cfa1644fe
Anton Batiaev <a.batyaev@gmail.com>
2018-06-11 20:36:14 +0300

    lesson5 homework

---
+++ stream1/src/com/batiaev/java3/lesson6/Lesson6.java
@@ -0,0 +1,10 @@
+package com.batiaev.java3.lesson6;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Lesson6 {
+    public static void main(String[] args) {
+        log.info("Hello world");
+    }
+}
