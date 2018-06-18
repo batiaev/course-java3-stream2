26af66dd5660fbffc83435f52c892b18ba696c35
Anton Batiaev <a.batyaev@gmail.com>
2018-06-18 23:52:58 +0300

    lesson 8

--- stream1/src/main/java/com/batiaev/java3/lesson8/Lesson8.java
+++ stream1/src/main/java/com/batiaev/java3/lesson8/Lesson8.java
@@ -1,9 +1,16 @@
 package com.batiaev.java3.lesson8;
 
 public class Lesson8 {
+
+    public static String VALUE = "4";
+    static {
+        System.out.println("Lesson 8");
+    }
     public static void main(String[] args) {
+
         Long a = 111L;
         Long b = 111L;
+        Integer intVal = 111;
         Long c = 222L;
         Long d = 222L;
         Long e = 112L;
@@ -11,7 +18,7 @@ public class Lesson8 {
         System.out.println(a == b);
         System.out.println(c == d);
         b = b + 1;
-        System.out.println(a);
+//        System.out.println(a);
         System.out.println(b == e);
 
     }
